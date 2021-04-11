062cafae28d346dd6e3436b0339dbbc031f9a5fc
au01gh <59246763+au01gh@users.noreply.github.com>
2021-04-11 12:19:59 -0700

    Add files via upload

--- SampleController.java
+++ SampleController.java
@@ -5,10 +5,11 @@ import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.MenuBar;
 import javafx.stage.Stage;
+
 
 public class SampleController {
 	
@@ -16,13 +17,17 @@ public class SampleController {
 	private Scene scene;
 	private Parent root;
 	
+	@FXML
+	private MenuBar myMenuBar;
+	
     @FXML
     void habits(ActionEvent event) throws IOException{
     	root = FXMLLoader.load(getClass().getResource("Sample2.fxml"));
-    	stage = (Stage)((Node)event.getSource()).getScene().getWindow();
     	scene = new Scene(root);
+    	stage = (Stage) myMenuBar.getScene().getWindow(); 	
     	stage.setScene(scene);
     	stage.show();
     }
-
 }
+
+
