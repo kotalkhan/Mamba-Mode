c4477998271de5f738683c72121654a9997ac001
au01gh <59246763+au01gh@users.noreply.github.com>
2021-04-10 23:25:51 -0700

    Add files via upload

--- SampleController.java
+++ SampleController.java
@@ -1,14 +1,18 @@
 package application;
 
+import java.io.IOException;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
 public class SampleController {
 
@@ -19,30 +23,35 @@ public class SampleController {
     private Label label;
     
     @FXML
+    //sets the drop down choices
     public void initialize() {
     	ObservableList <String> dropDown = FXCollections.observableArrayList("Habits", "Statistics", "Calendar", "Goals");
         mainMenu.setItems(dropDown);
     }
     
-//    don't think i need this
-//    @FXML
-//    void select(ActionEvent event) {
-//    	String s = mainMenu.getSelectionModel().getSelectedItem().toString();
-//    	label.setText(s);
-//    }
-//    
-//    @FXML
-//    private void mainChoice() {
-//    	if(mainMenu.getValue().equals("Habits")) {
-//    		handleHabit();
-//    	}
-//    }
-//    
-//    public void handleHabit() {
-//    	GridPane root = (GridPane)FXMLLoader.load(getClass().getResource("Sample2.fxml"));
-//    	Scene scene = new Scene(root,850,500);
-//    	Stage window = (Stage) mainChoice().getScene().getWindow();
-//    	root.setScene(scene);
-//		root.show();
-//    }
+    //selects the drop down option
+    @FXML
+    void select(ActionEvent event) throws IOException {
+    	String s = mainMenu.getSelectionModel().getSelectedItem().toString();
+    	//checks if habit is home to go back
+    	if(s.equals("Habits")) {
+//    		FXMLLoader loader = new FXMLLoader();
+//			loader.setLocation(Main.class.getResource("Sample2.fxml"));
+//			Parent root = (Parent)loader.load();
+//        	Scene scene = new Scene(root,850,500);
+//        	Stage x = new Stage();
+//        	x.setScene(scene);
+//    		x.show();
+    		
+    		Stage primary = new Stage();
+    		Parent root = FXMLLoader.load(getClass().getResource("Sample2.fxml"));
+    		primary.setScene(new Scene(root,850,500));
+    		primary.show();
+    	}
+    }
+    
+    //Goes to the add habit screen
+    public void handleHabit() {
+    	
+    }
 }
---
+++ SampleController2.java
@@ -0,0 +1,107 @@
+package application;
+
+import java.awt.Label;
+import java.io.IOException;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class SampleController2 {
+
+    @FXML
+    private ComboBox mainMenu;
+
+    @FXML
+    private TextField goal;
+
+    @FXML
+    private TextField habit;
+
+    @FXML
+    private CheckBox sun;
+
+    @FXML
+    private CheckBox mon;
+
+    @FXML
+    private CheckBox tue;
+
+    @FXML
+    private CheckBox wed;
+
+    @FXML
+    private CheckBox thu;
+
+    @FXML
+    private CheckBox fri;
+
+    @FXML
+    private CheckBox sat;
+
+    @FXML
+    private Button confirm;
+
+    @FXML
+    private Label label;
+
+    @FXML
+    //sends code to DataBase class to add to the database and clears values
+    void add(ActionEvent event) {
+    	clear();
+	
+    }
+
+    @FXML
+    //sets the drop down choices
+    public void initialize() {
+    	ObservableList <String> dropDown = FXCollections.observableArrayList("Home", "Statistics", "Calendar", "Goals");
+        mainMenu.setItems(dropDown);
+    }
+    
+    @FXML
+    //selects the drop down option
+    void select(ActionEvent event) throws IOException {
+    	String s = mainMenu.getSelectionModel().getSelectedItem().toString();
+    	//checks if habit is home to go back
+    	if(s.equals("Home")) {
+    		Stage primary = new Stage();
+    		Parent root = FXMLLoader.load(getClass().getResource("Sample1.fxml"));
+    		primary.setScene(new Scene(root,850,500));
+    		primary.show();
+    	}
+    }
+    
+    //clears all fields
+    void clear() {
+    	goal.clear();
+    	habit.clear();
+    	sun.setSelected(false);
+    	mon.setSelected(false);
+    	tue.setSelected(false);
+    	wed.setSelected(false);
+    	thu.setSelected(false);
+    	fri.setSelected(false);
+    	sat.setSelected(false);
+    }
+    
+    //Goes back to the home screen
+//    public void handleHome() {
+//    	GridPane root = (GridPane)FXMLLoader.load(getClass().getResource("Sample2.fxml"));
+//    	Scene scene = new Scene(root,850,500);
+//    	Stage window = (Stage) mainChoice().getScene().getWindow();
+//    	root.setScene(scene);
+//		root.show();
+//    }
+
+}
