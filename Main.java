11aad44833d088a091da06fbc27c5d638308ae21
au01gh <59246763+au01gh@users.noreply.github.com>
2021-04-18 18:19:13 -0700

    new fxml changes

--- Main.java
+++ Main.java
@@ -2,24 +2,19 @@ package application;
 	
 import javafx.application.Application;
 import javafx.stage.Stage;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.fxml.FXMLLoader;
 
 
 public class Main extends Application {
-	private Parent root;
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			FXMLLoader loader = new FXMLLoader();
-			loader.setLocation(Main.class.getResource("Sample1.fxml"));
-			root = (Parent)loader.load();
-			Scene scene = new Scene(root,850,500);			
-			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());		
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
+			Scene scene = new Scene(root,850,550);
 			primaryStage.setScene(scene);
-			primaryStage.show();	
+			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
@@ -28,5 +23,4 @@ public class Main extends Application {
 	public static void main(String[] args) {
 		launch(args);
 	}
-	
 }
--- SampleController.java
+++ SampleController.java
@@ -1,6 +1,7 @@
 package application;
 
-import java.awt.TextField;
+import java.io.IOException;
+import java.util.ArrayList;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -8,30 +9,130 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
 
 public class SampleController {
 
     @FXML
-    Button btn1;
-    Button btn2;
+    private Button btn1;
 
     @FXML
-    void handlebtn1() throws Exception {
-      	Parent root = FXMLLoader.load(getClass().getResource("dd.fxml"));
-    	Stage window = (Stage) btn1.getScene().getWindow(); 	
-    	window.setScene(new Scene(root, 850, 500));
+    private Button btn2;
+
+    @FXML
+    public void home2habits() throws Exception {
+    	Parent root = FXMLLoader.load(getClass().getResource("Sample3.fxml"));
+    	Stage window = (Stage) btn2.getScene().getWindow();
+    	window.setScene(new Scene(root, 850, 550));
+    }
+
+    @FXML
+    public void home2statistics() throws Exception {
+    	Parent root = FXMLLoader.load(getClass().getResource("Sample2.fxml"));
+    	Stage window = (Stage) btn1.getScene().getWindow();
+    	window.setScene(new Scene(root, 850, 550));
+    }
+
+    @FXML
+    private Button btn3;
+
+    @FXML
+    private Button btn4;
+
+    @FXML
+    public void statistics2habits() throws Exception {
+    	Parent root = FXMLLoader.load(getClass().getResource("Sample3.fxml"));
+    	Stage window = (Stage) btn4.getScene().getWindow();
+    	window.setScene(new Scene(root, 850, 550));
+    }
+
+    @FXML
+    public void statistics2home() throws Exception {
+    	Parent root = FXMLLoader.load(getClass().getResource("Sample.fxml"));
+    	Stage window = (Stage) btn3.getScene().getWindow();
+    	window.setScene(new Scene(root, 850, 550));
     }
     
     @FXML
-    void handlebtn2() throws Exception {
-      	Parent root = FXMLLoader.load(getClass().getResource("Sample1.fxml"));
-    	Stage window = (Stage) btn2.getScene().getWindow(); 	
-    	window.setScene(new Scene(root, 850, 500));
-    }  
+    private Button btn5;
 
+    @FXML
+    private Button btn6;
 
+    @FXML
+    public void habits2home() throws Exception {
+    	Parent root = FXMLLoader.load(getClass().getResource("Sample.fxml"));
+    	Stage window = (Stage) btn5.getScene().getWindow();
+    	window.setScene(new Scene(root, 850, 550));
+    }
+
+    @FXML
+    public void habits2statistics() throws Exception {
+    	Parent root = FXMLLoader.load(getClass().getResource("Sample2.fxml"));
+    	Stage window = (Stage) btn6.getScene().getWindow();
+    	window.setScene(new Scene(root, 850, 550));
+    }
+    
+    @FXML
+    private Button addy;
+    
+    @FXML
+    void addHabit(ActionEvent event) {	
+		try {
+			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("add.fxml"));
+	    	Parent root = (Parent) fxmlLoader.load();
+			Stage stage = new Stage();
+	    	stage.setScene(new Scene(root));
+	    	
+	    	stage.initModality(Modality.WINDOW_MODAL);
+	    	stage.initOwner(addy.getScene().getWindow());
+	    	stage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 	
+    }
+    
+    @FXML
+    private Button edity;
+    
+    @FXML
+    void editHabit(ActionEvent event) {	
+		try {
+			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("edit.fxml"));
+	    	Parent root = (Parent) fxmlLoader.load();
+			Stage stage = new Stage();
+	    	stage.setScene(new Scene(root));
+	    	
+	    	stage.initModality(Modality.WINDOW_MODAL);
+	    	stage.initOwner(edity.getScene().getWindow());
+	    	stage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 	
+    }
+    
+    @FXML
+    private Button deletey;
+    
+    @FXML
+    void deleteHabit(ActionEvent event) {	
+		try {
+			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("delete.fxml"));
+	    	Parent root = (Parent) fxmlLoader.load();
+			Stage stage = new Stage();
+	    	stage.setScene(new Scene(root));
+	    	
+	    	stage.initModality(Modality.WINDOW_MODAL);
+	    	stage.initOwner(deletey.getScene().getWindow());
+	    	stage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 	
+    }
+    
     @FXML
     private TextField habit;
 
@@ -56,21 +157,13 @@ public class SampleController {
     @FXML
     private CheckBox sat;
 
-	@FXML
-	void add(ActionEvent event) {
-		clearChecked();
-	}
-
-	void clearChecked() {
-		// habit.getEditor().clear();
-		sun.setSelected(false);
-		mon.setSelected(false);
-		tue.setSelected(false);
-		wed.setSelected(false);
-		thu.setSelected(false);
-		fri.setSelected(false);
-		sat.setSelected(false);
-
-	}
-
+    @FXML
+    void sendToAdd(ActionEvent event) {
+    	//get id, send to DB, clear
+    	Boolean [] week = new Boolean [6];
+     	if(sun.isSelected()) {
+    		week[0] = true;
+    	}
+    }
+    
 }
